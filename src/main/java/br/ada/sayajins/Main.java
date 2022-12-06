c06388189b36978b9804011a8991a68ac4ab026d
vagrant <vagrant@vagrant.vm>
2022-12-06 19:35:34 +0000

    implementado separacao de arquivos por tipo de pagamento

--- src/main/java/br/ada/sayajins/Main.java
+++ src/main/java/br/ada/sayajins/Main.java
@@ -2,18 +2,19 @@ package br.ada.sayajins;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import br.ada.sayajins.dao.PagamentosDao;
 import br.ada.sayajins.model.Pagamentos;
+import br.ada.sayajins.model.TipoPagamentoEnum;
 import br.ada.sayajins.service.PagamentosService;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, Exception {
 
         try {
-            File novoArquivo = new File("src\\main\\resources\\arquivoCriado.csv");
-
             PagamentosDao pDao = new PagamentosDao();
             List<Pagamentos> pagamentos = new ArrayList<Pagamentos>();
 
@@ -23,9 +24,21 @@ public class Main {
             PagamentosService.alterarListaAdiantado(pagamentos);
 
             pagamentos.stream().forEach(System.out::println);
-            pDao.criarArquivo(novoArquivo, pagamentos);
+            criarArquivosSeparados(pDao, pagamentos);
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    private static void criarArquivosSeparados(PagamentosDao pDao, List<Pagamentos> pagamentos) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        for (TipoPagamentoEnum values : TipoPagamentoEnum.values()) {
+            String fileName = "src/main/resources/PAGAMENTOS_" + values.name() + "_" + LocalDate.now().format(formatter)
+                    + ".csv";
+            File novoArquivo = new File(fileName);
+            List<Pagamentos> lista = PagamentosService.retornaPagamentosPorTipo(pagamentos, values);
+            pDao.criarArquivo(novoArquivo, lista);
         }
     }
 }
