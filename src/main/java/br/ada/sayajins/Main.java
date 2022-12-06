73a38261c9a075704c5ea079256cbb81513cfcac
vagrant <vagrant@vagrant.vm>
2022-12-06 19:40:43 +0000

    refactoring pagamentosDao

--- src/main/java/br/ada/sayajins/Main.java
+++ src/main/java/br/ada/sayajins/Main.java
@@ -1,14 +1,10 @@
 package br.ada.sayajins;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import br.ada.sayajins.dao.PagamentosDao;
 import br.ada.sayajins.model.Pagamentos;
-import br.ada.sayajins.model.TipoPagamentoEnum;
 import br.ada.sayajins.service.PagamentosService;
 
 public class Main {
@@ -24,21 +20,10 @@ public class Main {
             PagamentosService.alterarListaAdiantado(pagamentos);
 
             pagamentos.stream().forEach(System.out::println);
-            criarArquivosSeparados(pDao, pagamentos);
+            pDao.criarArquivosSeparados(pagamentos);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    private static void criarArquivosSeparados(PagamentosDao pDao, List<Pagamentos> pagamentos) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-        for (TipoPagamentoEnum values : TipoPagamentoEnum.values()) {
-            String fileName = "src/main/resources/PAGAMENTOS_" + values.name() + "_" + LocalDate.now().format(formatter)
-                    + ".csv";
-            File novoArquivo = new File(fileName);
-            List<Pagamentos> lista = PagamentosService.retornaPagamentosPorTipo(pagamentos, values);
-            pDao.criarArquivo(novoArquivo, lista);
-        }
-    }
 }
